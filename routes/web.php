3a7186b7e0a69158bea0220dd1cd1a02f5360c99
nogueript <nogueript@gmail.com>
2019-05-14 17:01:44 +0700

    web

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,7 @@
 use App\Notifications\TryNotify;
 use App\User;
 use Carbon\Carbon;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 Route::get('/', 'PagesController@getIndex');
 
 Route::get('/imlost', 'PagesController@getImLost');
@@ -35,7 +26,3 @@ Route::post('/send', 'ChatController@send');
 Route::post('/contact/submit', 'MessagesController@submit');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-
