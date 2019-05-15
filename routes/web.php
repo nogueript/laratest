97f4101ab34dcbb297a5d36eb0dc029bfefc4223
nogueript <nogueript@gmail.com>
2019-05-15 16:17:04 +0700

    repair route

--- routes/web.php
+++ routes/web.php
@@ -1,74 +1,20 @@
-<<<<<<< HEAD
 <?php
 
-use App\Notifications\TryNotify;
 use App\User;
 use Carbon\Carbon;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 Route::get('/', 'PagesController@getIndex');
-
 Route::get('/imlost', 'PagesController@getImLost');
-
 Route::get('/haha', 'PagesController@getHaha');
-
 Route::get('maskAsRead', function(){
     auth()->user()->unreadNotifications->markAsRead();
     return redirect()->back();
 })->name('markRead');
-
 Route::get('/contact', 'PagesController@getContact');
-
 Route::get('/obj', 'PagesController@getObj');
-
 Route::get('/about', 'PagesController@getAbout');
-
 Route::get('chat', 'ChatController@chat');
-
 Route::post('send', 'ChatController@send');
-
 Route::post('/contact/submit', 'MessagesController@submit');
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
-
-
-=======
-<?php
-
-use App\Notifications\TryNotify;
-use App\User;
-use Carbon\Carbon;
-
-Route::get('/', 'PagesController@getIndex');
-
-Route::get('/imlost', 'PagesController@getImLost');
-
-Route::get('maskAsRead', function(){
-    auth()->user()->unreadNotifications->markAsRead();
-    return redirect()->back();
-})->name('markRead');
-
-Route::get('/contact', 'PagesController@getContact');
-
-Route::get('/obj', 'PagesController@getObj');
-
-Route::get('/about', 'PagesController@getAbout');
-
-Route::get('/chat', 'ChatController@chat');
-
-Route::post('/send', 'ChatController@send');
-
-Route::post('/contact/submit', 'MessagesController@submit');
-
-Auth::routes();
->>>>>>> 3a7186b7e0a69158bea0220dd1cd1a02f5360c99
